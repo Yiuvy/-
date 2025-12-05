fb4261858d75fdff5736d9e56a2e13c37ae1d7a5
Yiuvy <Odelia_may@mail.ru>
2025-12-05 15:40:15 +0300

    Ex.5.4 Image tag-helper

---
+++ Makarevich_proj/TagHelpers/ImageTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Makarevich_proj.TagHelpers
+{
+    [HtmlTargetElement("img", Attributes = "img-action, img-controller")]
+    public class ImageTagHelper(LinkGenerator linkGenerator) : TagHelper
+    {
+        public string ImgController { get; set; }
+        public string ImgAction { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("src",
+            linkGenerator.GetPathByAction(ImgAction, ImgController));
+
+        }
+    }
+}

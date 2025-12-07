d5829aee91fd399d745a40860d332fa5aa91628f
Yiuvy <Odelia_may@mail.ru>
2025-12-07 15:10:13 +0300

    Ex 3.8. APIController corrected methods

---
+++ Makarevich_sol_API/Controllers/ClinicsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Makarevich_sol_API.Data;
+using Makarevich_sol_Domain.Entities;
+using Makarevich_sol_Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Makarevich_sol_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicsController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public ClinicsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clinics
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clinic>>> GetClinics()
+        {
+            var response = new ResponseData<IEnumerable<Clinic>>
+            {
+                Data = await _context.Clinics.ToListAsync()
+            };
+             return Ok(response);
+
+            //return Ok(await _context.Clinics.ToListAsync());
+            //return await _context.Clinics.ToListAsync();
+        }
+
+        // GET: api/Clinics/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clinic>> GetClinic(int id)
+        {
+            var clinic = await _context.Clinics.FindAsync(id);
+
+            if (clinic == null)
+            {
+                return NotFound();
+            }
+
+            return clinic;
+        }
+
+        // PUT: api/Clinics/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClinic(int id, Clinic clinic)
+        {
+            if (id != clinic.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(clinic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClinicExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clinics
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Clinic>> PostClinic(Clinic clinic)
+        {
+            _context.Clinics.Add(clinic);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClinic", new { id = clinic.Id }, clinic);
+        }
+
+        // DELETE: api/Clinics/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClinic(int id)
+        {
+            var clinic = await _context.Clinics.FindAsync(id);
+            if (clinic == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clinics.Remove(clinic);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClinicExists(int id)
+        {
+            return _context.Clinics.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Makarevich_sol_API/Controllers/DoctorsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Makarevich_sol_API.Data;
+using Makarevich_sol_Domain.Entities;
+using Makarevich_sol_Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Makarevich_sol_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+        private readonly IWebHostEnvironment _env;
+        List<Doctor> _doctors;
+        List<Clinic> _clinics;
+        private readonly IConfiguration _config;
+        public DoctorsController(AppDBContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        // GET: api/Doctors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors()
+        {
+
+            return await _context.Doctors.ToListAsync();
+        }
+
+        // GET: api/Doctors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Doctor>> GetDoctor(string? ClinicNormalizedName,
+            int pageNo = 1,
+            int pageSize = 3)
+        {
+           { // Создать объект результата
+            var result = new ResponseData<ListModel<Doctor>>();
+
+            int? clinicId = null;
+
+            // если требуется фильтрация, то найти Id категории
+            // с заданным categoryNormalizedName
+
+            if (ClinicNormalizedName != null)
+                clinicId = _clinics
+                .Find(c => c.IdNormalizedName.Equals(ClinicNormalizedName))
+                ?.Id;
+
+            // Выбрать объекты, отфильтрованные по Id категории,
+            // если этот Id имеется
+            var data = _doctors
+            .Where(d => clinicId == null || d.IdClinic.Equals(clinicId))?
+            .ToList();
+
+
+            // получить размер страницы из конфигурации
+            int pgSize = _config.GetSection("ItemsPerPage").Get<int>();
+            // получить общее количество страниц
+            int totalPages = (int)Math.Ceiling(data.Count / (double)pageSize);
+
+            // получить данные страницы
+            var listData = new ListModel<Doctor>()
+            {
+                Items = data.Skip((pageNo - 1) * pageSize).Take(pageSize).ToList(),
+                CurrentPage = pageNo,
+                TotalPages = totalPages
+            };
+
+            // поместить данные в объект результата
+            result.Data = listData;
+
+            // Если список пустой
+            if (data.Count == 0)
+            {
+                result.Success = false;
+                result.ErrorMessage = "Нет объектов в выбраннной категории";
+            }
+
+            return Ok(result);}
+
+        }
+
+        // PUT: api/Doctors/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDoctor(int id, Doctor doctor)
+        {
+            if (id != doctor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(doctor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Doctors
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Doctor>> PostDoctor(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDoctor", new { id = doctor.Id }, doctor);
+        }
+
+        // DELETE: api/Doctors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDoctor(int id)
+        {
+            var doctor = await _context.Doctors.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Doctors.Remove(doctor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return _context.Doctors.Any(e => e.Id == id);
+        }
+    }
+}

a2fdb5fed8ead21afa4cab704547a7d6624bb6eb
Yiuvy <Odelia_may@mail.ru>
2025-12-12 16:31:04 +0300

    ProductControllerTest Done and passed

---
+++ Makarevich_sol_TEST/DishesControllerTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure;
+using Makarevich_proj.Controllers;
+using Makarevich_sol_Domain.Entities;
+using Makarevich_sol_Domain.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using Xunit;
+
+namespace Makarevich_sol_TEST
+{
+    public class DishesControllerTests : IDisposable
+    {
+        //    private readonly DbConnection _connection;
+        //    private readonly DbContextOptions<ProductDbContext> _contextOptions;
+        //    private readonly IWebHostEnvironment _environment;
+        //    public DishesControllerTests()
+        //    {
+        //        _environment = Substitute.For<IWebHostEnvironment>();
+
+        //        // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+        //        // at the end of the test (see Dispose below).
+        //        _connection = new SqliteConnection("Filename=:memory:");
+        //        _connection.Open();
+
+        //        // These options will be used by the context instances in this test suite, including the connection opened above.
+        //        _contextOptions = new DbContextOptionsBuilder<ProductDbContext>()
+        //        .UseSqlite(_connection)
+        //        .Options;
+
+        //        // Create the schema and seed some data
+        //        using var context = new ProductDbContext(_contextOptions);
+        //        context.Database.EnsureCreated();
+        //        var categories = new Clinic[]
+        //        {
+        //            new Clinic {Name="", IdNormalizedName="Merci"},
+        //            new Clinic {Name="", IdNormalizedName="MedAvenue"}
+        //        };
+
+        //        context.Categories.AddRange(categories);
+        //        context.SaveChanges();
+
+        //        var dishes = new List<Doctor>
+        //        {
+        //            new Doctor {
+        //                Name="", Surname="",AmountOfPatients=0,
+        //               IdClinic=categories.FirstOrDefault(c=>c.IdNormalizedName.Equals("Merci")).Id },
+
+        //            new Doctor {
+        //                Name="",Surname="", AmountOfPatients=0,
+        //               IdClinic=categories.FirstOrDefault(c=>c.IdNormalizedName.Equals("MedAvenue")).Id },
+
+        //            new Doctor {
+        //                Name="",Surname="",AmountOfPatients=0,
+        //               IdClinic=categories.FirstOrDefault(c=>c.IdNormalizedName.Equals("MedAvenue")).Id },
+
+        //            new Doctor {
+        //                Name="",Surname="",AmountOfPatients=0,
+        //               IdClinic=categories.FirstOrDefault(c=>c.IdNormalizedName.Equals("Merci")).Id },
+
+        //             new Doctor {
+        //                Name="",Surname="",AmountOfPatients=0,
+        //               IdClinic=categories.FirstOrDefault(c=>c.IdNormalizedName.Equals("MedAvenue")).Id },
+        //        };
+        //        context.AddRange(dishes);
+        //        context.SaveChanges();
+        //    }
+        //    public void Dispose() => _connection?.Dispose();
+        //    ProductDbContext CreateContext() => new ProductDbContext(_contextOptions);
+
+        //    // Проверка фильтра по категории [Fact]
+        //    public async void ControllerFiltersCategory()
+        //    {
+        //        // arrange
+        //        using var context = CreateContext();
+        //        var category = context.Categories.First();
+        //        var controller = new DishesController(context, _environment);
+        //        // act
+        //        var response = await controller.GetDoctor(category.NormalizedName); 
+        //        ResponseData<ListModel<Doctor>> responseData = response.Value;
+        //        var dishesList = responseData.Data.Items; // полученный список объектов
+
+        //        //assert
+        //        Assert.True(dishesList.All(d => d.IdClinic == category.Id));
+        //    }
+
+        //    // Проверка подсчета количества страниц
+        //    // Первый параметр - размер страницы
+        //    // Второй параметр - ожидаемое количество страниц (при условии, что всего объектов 5)
+        //    [Theory]
+        //    [InlineData(2, 3)]
+        //    [InlineData(3, 2)]
+        //    public async void ControllerReturnsCorrectPagesCount(int size, int qty)
+        //    {
+        //        using var context = CreateContext();
+        //        var controller = new DishesController(context, _environment);
+
+
+        //        // act
+        //        var response = await controller.GetDishes(null, 1, size);
+        //        ResponseData<ListModel<Doctor>> responseData = response.Value;
+        //        var totalPages = responseData.Data.TotalPages; // полученное количество
+
+
+        //        //assert
+        //        Assert.Equal(qty, totalPages); // количество страниц совпадает
+        //    }
+
+        //    [Fact]
+        //    public async void ControllerReturnsCorrectPage()
+        //    {
+        //        using var context = CreateContext();
+        //        var controller = new DishesController(context, _environment);
+        //        // При размере страницы 3 и общем количестве объектов 5
+        //        // на 2-й странице должно быть 2 объекта
+        //        int itemsInPage = 2;
+        //        // Первый объект на второй странице
+        //        Doctor firstItem = context.Dishes.ToArray()[3];
+
+
+        //        // act
+        //        // Получить данные 2-й страницы
+        //        var response = await controller.GetDishes(null, 2);
+        //        ResponseData<ListModel<o>> responseData = response.Value;
+        //        var dishesList = responseData.Data.Items; // полученный список объектов
+        //        var currentPage = responseData.Data.CurrentPage; // полученный номер текущей  страницы
+
+
+        //        //assert
+        //        Assert.Equal(2, currentPage);// номер страницы совпадает
+        //        Assert.Equal(2, dishesList.Count); // количество объектов на странице равно  2
+        //    Assert.Equal(firstItem.Id, dishesList[0].Id); // 1-й объект в списке     правильный
+        //    }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
---
+++ Makarevich_sol_TEST/ProductControllerTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Makarevich_proj.Controllers;
+using Makarevich_proj.Services.contracts_interfaces_;
+using Makarevich_sol_Domain.Entities;
+using Makarevich_sol_Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Xunit;
+
+namespace Makarevich_sol_TEST
+{
+    public class ProductControllerTests
+    {
+        IProductService _productService;
+        ICategoryService _categoryService;
+        public ProductControllerTests()
+        {
+            SetupData();
+        }
+        // Список категорий сохраняется во ViewData
+
+        [Fact]
+        public async void IndexPutsCategoriesToViewData()
+        {
+            //arrange
+            var controller = new ProductController(_categoryService, _productService);
+            //act
+            var response = await controller.Index(null);
+            //assert
+            var view = Assert.IsType<ViewResult>(response);
+            var categories = Assert.IsType<List<Clinic>>(view.ViewData["clinics"]);
+            Assert.Equal(2, categories.Count);
+            Assert.Equal("Все клиники", view.ViewData["currentClinic"]);
+        }
+
+        // Имя текущей категории сохраняется во ViewData
+        //
+        [Fact]
+        public async void IndexSetsCorrectCurrentClinic()
+        {
+            //arrange
+            var categories = await _categoryService.GetCategoryListAsync();
+            var currentCategory = categories.Data[0];
+            var controller = new ProductController(_categoryService, _productService);
+            //act
+            var response = await controller.Index(currentCategory.IdNormalizedName);
+            //assert
+            var view = Assert.IsType<ViewResult>(response);
+
+            Assert.Equal(currentCategory.Name, view.ViewData["currentClinic"]);
+        }
+
+
+        // В случае ошибки возвращается NotFoundObjectResult
+        //
+        [Fact]
+        public async void IndexReturnsNotFound()
+        {
+            //arrange
+            string errorMessage = "Test error";
+            var categoriesResponse = new ResponseData<List<Clinic>>();
+            categoriesResponse.Success = false; categoriesResponse.ErrorMessage = errorMessage;
+            _categoryService.GetCategoryListAsync().Returns(Task.FromResult(categoriesResponse));
+            var controller = new ProductController(_categoryService, _productService);
+            //act
+            var response = await controller.Index(null);
+            //assert
+            var result = Assert.IsType<NotFoundObjectResult>(response);
+            Assert.Equal(errorMessage, result.Value.ToString());
+
+        }
+        // Настройка имитации ICategoryService и IProductService
+        void SetupData()
+        {
+            _categoryService = Substitute.For<ICategoryService>();
+            var categoriesResponse = new ResponseData<List<Clinic>>();
+            categoriesResponse.Data = new List<Clinic>
+            {
+                new Clinic {Id=1,    Name="ООО Кравира",IdNormalizedName="Kravira"},
+                new Clinic {Id=2, Name="ЗАО Лодэ",IdNormalizedName="Lode"}  
+            };
+
+
+            _categoryService.GetCategoryListAsync().Returns(Task.FromResult(categoriesResponse));
+            _productService = Substitute.For<IProductService>();
+            
+            var dishes = new List<Doctor>
+            {
+                new Doctor {Id = 1 },
+                new Doctor { Id = 2 },
+                new Doctor { Id = 3 },
+                new Doctor { Id = 4 },
+                new Doctor { Id = 5 }
+            };
+
+            var productResponse = new ResponseData<ListModel<Doctor>>();
+            productResponse.Data = new ListModel<Doctor> { Items = dishes };
+            _productService.GetProductListAsync(Arg.Any<string?>(), Arg.Any<int>()).Returns(productResponse);
+        }
+    }
+
+
+
+
+
+}

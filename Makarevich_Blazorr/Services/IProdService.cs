8fa1b62b225887bbd8c770773b417def988a3c0e
Yiuvy <Odelia_may@mail.ru>
2025-12-18 10:41:26 +0300

    Опишите обобщенный (generic) интерфейс IProductService, предоставляющий следующее: - метод для получения списка - список объектов, - номер текущей страницы - общее количество страниц - событие (сообщение о том, что список изменился)

---
+++ Makarevich_Blazorr/Services/IProdService.cs
@@ -0,0 +1,16 @@
+﻿namespace Makarevich_Blazorr.Services
+{
+    public interface IProdService<T> where T : class
+    {
+        event Action ListChanged;
+        // Список объектов
+        IEnumerable<T> Products { get; }
+        // Номер текущей страницы
+        int CurrentPage { get; }
+        // Общее количество страниц
+        int TotalPages { get; }
+        // Получение списка объектов
+        Task GetProducts(int pageNo = 1, int pageSize = 3);
+
+    }
+}

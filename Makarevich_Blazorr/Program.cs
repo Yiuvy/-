9ba02cc445bc1c002874732a00599ec4f4fe759d
Yiuvy <Odelia_may@mail.ru>
2025-12-18 10:57:19 +0300

    Зарегистрируйте в классе Program фабрику HttpClient для созданного сервиса. Внедрите созданный сервис на страницу Products

--- Makarevich_Blazorr/Program.cs
+++ Makarevich_Blazorr/Program.cs
@@ -1,10 +1,15 @@
 using Makarevich_Blazorr.Components;
+using Makarevich_Blazorr.Services;
+using Makarevich_sol_Domain.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services
+.AddHttpClient<IProdService<Doctor>, ApiProdService>(c => c.BaseAddress = new Uri("https://localhost:7002/api/dishes"));
+
 
 var app = builder.Build();
 
